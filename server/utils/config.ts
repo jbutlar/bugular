b25312eb1e20ba8f0819fb152eb70bf640750353
jbutlar <jgbutlar@gmail.com>
2020-07-14 23:14:58 -0400

    removing prod consideration for env variable

--- server/utils/config.ts
+++ server/utils/config.ts
@@ -2,10 +2,9 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-
 export const port = process.env.NODE_ENV === 'production' ? process.env.PORT : process.env.SERVER_PORT
 export const baseUrl = process.env.NODE_ENV === 'production' ? 'https://bugular.herokuapp.com/' : process.env.BASE_URL
-export const MYSQL_DB_CONFIG = process.env.NODE_ENV === 'production' ? process.env.JAWSDB_URL : {
+export const MYSQL_DB_CONFIG = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
